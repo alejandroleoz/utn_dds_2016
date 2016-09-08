54b4888328f3d786e83a0c5ed49958aa27810bc9
Alejandro Ezequiel Leoz <alejandro.leoz@sovanta.com>
2016-09-07 23:06:28 -0300

    Entrega 2: Test buscador (falta mockear el servicio externo)

---
+++ src/test/java/utn/dds/tp/search/TestBuscador.java
@@ -0,0 +1,51 @@
+package utn.dds.tp.search;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import utn.dds.tp.Flyweight;
+import utn.dds.tp.datasource.BancoAdapter;
+import utn.dds.tp.datasource.CGPAdapter;
+import utn.dds.tp.manager.POIManager;
+import utn.dds.tp.poi.POI;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Created by aleoz on 9/7/16.
+ */
+public class TestBuscador {
+
+    @Test
+    public void test_busquedaRemota(){
+
+        // buscador concreto
+        BuscadorConcreto buscador = new BuscadorConcreto();
+
+        // timer
+        TimerProxy timer = new TimerProxy();
+        timer.setBuscador(buscador);
+
+        // historico Proxy
+        HistoricoProxy historicoProxy = new HistoricoProxy();
+        historicoProxy.setBuscador(timer);
+
+        POIManager poiManager = Flyweight.getInstance().getPoiManager();
+        poiManager.setFuentesExternas(Arrays.asList(new BancoAdapter(), new CGPAdapter()));
+        poiManager.setBuscador(historicoProxy);
+
+        // TODO: habría que Mockear el servicio externo!!
+
+        Collection<POI> pois = poiManager.buscar("tand");
+        assertEquals("Busqueda banco Santander", 1, pois.size());
+
+        pois = poiManager.buscar("magro");
+        assertEquals("Busqueda centro Almagro", 1, pois.size());
+
+        pois = poiManager.buscar("");
+        assertTrue("Busqueda (todos los resultados)", pois.size() > 1);
+
+        pois = poiManager.buscar("saraza");
+        assertEquals("Busqueda 0 resultados", 0, pois.size());
+    }
+}
